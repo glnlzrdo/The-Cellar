6de9324a1d35ea357a98b16f0e8f98486285710b
Glenn Lizardo <gdlizardo@up.edu.ph>
2016-12-30 12:27:31 +0800

    Added Files to Repository

---
+++ scripts/liquors.js
@@ -0,0 +1,194 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {  
+  liquor1 : {
+    name : 'Alfonso I',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Falfonso.jpg?alt=media&token=3bbb83a6-9658-4e61-a7ab-ddfa25629b81',
+    desc : 'Brandy prepared with medium and high-strength wine spirits, 77% and 94% Alc./Vol. aged in oak casks.',
+    price : 824,
+    status : 'available'
+  },
+
+  liquor2 : {
+    name : 'Bacardi Superior Gold Carta Oro',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fbacardi-gold.jpg?alt=media&token=384b54d4-7b3e-428f-bada-f0f406807a08',
+    desc : 'Now relaunched at a higher strength of 40%, Bacardi Oro is a distinctive golden rum with a smooth taste and subtle mellow characteristics, this is ideal served long over ice with cola or fruit juices.',
+    price : 924,
+    status : 'available'
+  },
+
+  liquor3 : {
+    name : 'J&B Rare',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fj-b.jpg?alt=media&token=0b42e780-4933-4a40-a2df-3e1fa6236de9',
+    desc : 'Great in a cocktail, the unique blend makes it perfect for mixing – so by all means go overboard on the mixers, but please don’t go overboard on the J&B Scotch whisky.',
+    price : 1024,
+    status : 'available'
+  },
+
+  liquor4 : {
+    name : 'Martini Bianco',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fmartini-bianco.jpg?alt=media&token=afcdc6cd-ff7a-436a-8207-06b409eb5be0',
+    desc : 'Martini Bianco is a fine blend of Italian white wine infused with herbs and sweet floral botanicals and produces an aromatic yet delicate drink with notes of vanilla and citrus.',
+    price : 818,
+    status : 'available'
+  },
+
+  liquor5 : {
+    name : 'Smirnoff Red',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fsmirnoff.jpg?alt=media&token=743cc6df-00b9-433e-b48f-f1a11b8165d3',
+    desc : 'Smirnoff VODKA is the largest vodka brand in the world. It is an ultra smooth vodka with a classic taste that has inspired other varieties of vodkas worldwide.',
+    price : 624,
+    status : 'available'
+  },
+
+  liquor6 : {
+    name : 'Jose Cuervo Gold Especial Reposado',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fjose-cuervo-gold.jpg?alt=media&token=2615751b-44f9-4442-8a1d-803930b7e712',
+    desc : 'Cuervo Gold is golden-style joven tequila made from a blend of reposado (aged) and younger tequilas. Ever the story-maker, Cuervo Gold’s own story includes the leading role in the invention of The Margarita, and it is still the perfect tequila for that beloved cocktail.',
+    price : 933,
+    status : 'available'
+  },
+
+  liquor7 : {
+    name : 'Johnnie Walker Red Label',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fjw-red-label.jpg?alt=media&token=387361ef-8817-43a0-b66a-f8d8e8885cb9',
+    desc : 'Johnnie Walker Red Label was launched in its current form by brothers George and Alexander Walker in 1909, though it had existed as Walker\'s Special Old Highland Red Label for some years previously.',
+    price : 518,
+    status : 'unavailable'
+  },
+
+  liquor8 : {
+    name : 'Cutty Sark',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fcutty-sark.jpg?alt=media&token=7f281dca-4a72-4fc8-b700-834fee94a418',
+    desc : 'Named for the historic ship, Cutty Sark is a blend that\'s been around since 1923. It has whisky from Glenrothes at its core along with whisky from all over Scotland to give a rounded blend with a little bit of smoke.',
+    price : 803,
+    status : 'available'
+  },
+
+  liquor9 : {
+    name : 'Malibu',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fmalibu.jpg?alt=media&token=1124498d-6aa2-4e33-8b0b-316d745eedd7',
+    desc : 'On the nose you get a mellow hit of coconut, whilst on the palate you receive a very sweet mix of fresh tropical fruits and coconut mixing together and forming a rather pleasant after-taste that doesn’t give that suspected ‘burn’.',
+    price : 720,
+    status : 'available'
+  },
+
+  liquor10 : {
+    name : 'Alizé Wild Passion',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Faliz%C3%A9.jpg?alt=media&token=07b3d439-d83f-4066-9b38-a9e3648847ff',
+    desc : 'Alize Wild Passion combines the fantastic Alize V.S Cognac with juice from grapefruit, mango and passionfruit. This heady blend makes a host of amazing cocktails.',
+    price : 924,
+    status : 'available'
+  },
+
+  liquor11 : {
+    name : 'Ballantine\'s Finest',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fballantines-finest.jpg?alt=media&token=e11bdc3b-364b-4bd0-bebf-baf427e0babd',
+    desc : 'Blended from more than 50 single malts (with particular emphasis on Miltonduff and Glenburgie) as well as 4 single grain whiskies. Ballantine\'s Finest is a smooth, satisfyingly modern blend, now picking up serious momentum in overseas markets under owners Chivas Brothers.',
+    price : 1024,
+    status : 'unavailable'
+  },
+
+  liquor12 : {
+    name : 'Hennessy XO 5cl Miniature Cognac',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fhennessy-xo-miniature.jpg?alt=media&token=c1b071b0-58c6-4c19-84df-2fe1bbe34c66',
+    desc : 'Its intense and deep amber color is a sign of its strength. Its flavors of candied fruit and its subtle and lightly spicy nose create a complete harmony: a mark of its roundness.',
+    price : 1324,
+    status : 'available'
+  },
+
+  liquor13 : {
+    name : 'Crown Royal',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fcrown-royal.jpg?alt=media&token=e8fb5213-102d-47d6-985f-0215c3b3813b',
+    desc : 'One of Canada\'s biggest and best known brands of whisky, Crown Royal is becoming increasingly known around the world.',
+    price : 1118,
+    status : 'available'
+  },
+
+  liquor14 : {
+    name : 'Jose Cuervo 1800 Silver',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fcuervo-1800-silver.jpg?alt=media&token=abaf6175-90df-4a9d-b9ca-c49a7c44f6b6',
+    desc : 'Jose Cuervo 1800 silver is a 100% blue agave white tequila named for the year in which tequila was first sold - 1800. It has an aroma of agave, roasted pepper, spice, and black peppercorns.',
+    price : 1024,
+    status : 'available'
+  },
+
+  liquor15 : {
+    name : 'Jack Daniel\'s Black Label',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fjack-daniel-s-black-label-70cl.jpg?alt=media&token=13a381b4-b108-43ff-8fd3-bdcdf6664032',
+    desc : 'Well worth the extra, Jack Daniel\'s Black Label is charcoal-mellowed twice, before and after the ageing process, resulting in a cleaner, more refined end product. Packaged in a smart monogram-esque bottle.',
+    price : 1324,
+    status : 'available'
+  },  
+
+  liquor16 : {
+    name : 'Chivas Regal 12 Year Old',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fchivas.jpg?alt=media&token=2f7fb070-602c-4780-83f3-ac58ae096095',
+    desc : 'Created from an exceptional blend of rare whiskies and aged in sherry casks from the Oloroso in Spain - it’s a unique new blend.',
+    price : 1824,
+    status : 'available'
+  },
+
+  liquor17 : {
+    name : 'Johnnie Walker Gold 18 Year Old',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fjw-gold-label.jpg?alt=media&token=5619965a-b901-4d1c-a97e-02111d78c902',
+    desc : 'Johnnie Walker Gold Label was relaunched in 2012. Built around a core of single malt whisky from Clynelish, the Gold Label Reserve is a great addition to the line-up from master blender Jim Beveridge.',
+    price : 1924,
+    status : 'available'
+  },
+
+  liquor18 : {
+    name : 'Rittenhouse',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Frittenhouse.jpg?alt=media&token=bcf80945-04be-4ca2-895c-84c4b43f1ef6',
+    desc : 'Very popular with rye aficionados, this must be tried by American whiskey enthusiasts.',
+    price : 1820,
+    status : 'available'
+  },
+
+  liquor19 : {
+    name : 'Johnnie Walker Black 12 Year Old',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fjw-black-label.jpg?alt=media&token=be71ea82-1d62-4976-ba0d-891919a46794',
+    desc : 'Black Label boasts superior depth and complexity, with a smokier, fuller flavor than other deluxe blends. Its distinctive character stems from its use of a remarkably diverse range of malts, drawn from over 700 casks.',
+    price : 1003,
+    status : 'available'
+  },
+
+  liquor20 : {
+    name : 'Arbor Mist Strawberry White Zinfandel',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Farbor-mist.jpg?alt=media&token=359f010c-3f3f-4ee6-828e-431e5696f319',
+    desc : 'A total berry delight. Bursting with natural fruit flavors, this wine is balanced and slightly sweet.',
+    price : 824,
+    status : 'available'
+  },
+
+  liquor21 : {
+    name : 'Squawking Magpie Stoned',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fsquawking-magpie-stoned.jpg?alt=media&token=8d404204-a5a3-41d5-994f-08df01334d5a',
+    desc : 'Dark oak concentrated derived from aging in French oak. Showing peppery and toasty characters, this wine has supple tannins with a firm, smooth and spicy finish. A serious single vineyard wine.',
+    price : 2324,
+    status : 'available'
+  },
+
+  liquor22 : {
+    name : 'Champagne - Moët & Chandon Brut Impérial',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fchampagne-moet-chandon.jpg?alt=media&token=1c996a42-974e-404a-a071-d73f27782ce2',
+    desc : 'Vibrant, generous and alluring, Moët Impérial is the House\'s iconic champagne. Created in 1869, it embodies Moët & Chandon\'s unique style, a style distinguished by its bright fruitiness, its seductive palate and its elegant maturity.',
+    price : 2824,
+    status : 'available'
+  },
+
+  liquor23 : {
+    name : 'Château Haut-Brion 1995',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fchateau-haut-brion.jpg?alt=media&token=6011b7e7-18eb-471a-88e5-2417212f39b6',
+    desc : 'Its depth, its roundness, its rich aroma with smoky characteristics of Pessac-Léognan, the elegance of its tannins, long finish...',
+    price : 3324,
+    status : 'available'
+  },
+
+  liquor24 : {
+    name : 'Hennessy Paradis Imperial',
+    image : 'https://firebasestorage.googleapis.com/v0/b/appdevbuddy.appspot.com/o/the-cellar%2Fimages%2Fhennessy-paradis.jpg?alt=media&token=5e47d656-f3eb-43e6-8392-b1ba7661703a',
+    desc : 'Its intense and deep amber color is a sign of its strength. Its flavors of candied fruit and its subtle and lightly spicy nose create a complete harmony: a mark of its roundness.',
+    price : 13324,
+    status : 'available'
+  }
+};
---
+++ scripts/main.js
@@ -0,0 +1,340 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CSSTransitionGroup = require('react-addons-css-transition-group');
+
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var History = ReactRouter.History;
+
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers')
+
+/*
+  Firebase
+*/
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://appdevbuddy.firebaseio.com/');
+
+
+var Catalyst = require('react-catalyst');
+
+/*
+  App
+*/
+
+var App = React.createClass({
+  mixins : [Catalyst.LinkedStateMixin],
+  getInitialState : function() {
+    return {
+      liquors : {},
+      order : {}
+    }
+  },
+  componentDidMount : function(){
+    //Pull Data from firebase
+    base.syncState(this.props.params.storeId + '/liquors', {
+      context : this,
+      state : 'liquors'
+    });
+
+    var localStorageRef = localStorage.getItem('order' + this.props.params.storeId);
+    if(localStorageRef) {
+      this.setState({
+        order : JSON.parse(localStorageRef)
+      });
+    }
+  },
+  componentWillUpdate : function(nextProps, nextState){
+    localStorage.setItem('order' + this.props.params.storeId, JSON.stringify(nextState.order));
+  },
+  addToBarrel : function(key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order : this.state.order });
+  },
+  removeFromBarrel : function(key) {
+    delete this.state.order[key];
+    this.setState({
+      order : this.state.order
+    });
+  },
+  addLiquor : function(liquor) {
+    var timestamp = (new Date()).getTime();
+    // update the state object
+    this.state.liquors['liquor-' + timestamp] = liquor;
+    // set the state
+    this.setState({ liquors : this.state.liquors });
+  },
+
+  removeLiquor : function(key) {
+    if (confirm("Are you sure you want to remove this liquor?!")) {
+      this.state.liquors[key] = null;
+      this.setState({
+        liquors : this.state.liquors
+      });
+    }
+  },
+  loadSamples : function() {
+    this.setState({
+      liquors : require('./liquors')
+    });
+  },
+  renderLiquor : function(key) {
+    return <Liquor key={key} index={key} details={this.state.liquors[key]} addToBarrel={this.addToBarrel}/>
+  },
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Dark, yet Shining" />
+          <ul className="list-of-liquors">
+            {Object.keys(this.state.liquors).map(this.renderLiquor)}
+          </ul>
+        </div>  
+        <Barrel liquors={this.state.liquors} 
+          order={this.state.order} 
+          removeFromBarrel={this.removeFromBarrel}/>
+        <Inventory 
+          addLiquor={this.addLiquor} 
+          loadSamples={this.loadSamples} 
+          liquors={this.state.liquors} 
+          linkState={this.linkState} 
+          removeLiquor={this.removeLiquor}/>
+      </div>
+    )
+  }
+});
+
+/*
+  Liquor Component
+*/
+
+var Liquor = React.createClass({
+  onButtonClick : function() {
+    var key = this.props.index;
+    this.props.addToBarrel(this.props.index);
+  },
+  render : function() {
+    var details = this.props.details;
+    var isAvailable = (details.status === 'available' ? true : false);
+    var buttonText = (isAvailable ? 'Add To Barrel' : 'Sold Out');
+    return (
+      <li className="menu-liquor">
+        <image src={details.image} alt={details.name} />
+        <h3 className="liquor-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
+      </li>
+    )
+  }
+});
+
+/*
+  Add Liquor Form
+*/
+
+var AddLiquorForm = React.createClass({
+  createLiquor : function(event) {
+    // Stop the form from submitting
+    event.preventDefault();
+    // Take the data from the form and create an object
+    var liquor = {
+      name : this.refs.name.value,
+      price : this.refs.price.value,
+      status : this.refs.status.value,
+      desc : this.refs.desc.value,
+      image : this.refs.image.value
+    }
+    // Add the liquor to the App State
+    this.props.addLiquor(liquor);
+    this.refs.liquorForm.reset();
+  },
+  render : function() {
+    return (
+        <form className="liquor-edit" ref="liquorForm" onSubmit={this.createLiquor}>
+          <input type="text" ref="name" placeholder="Liquor Name"/>
+          <input type="text" ref="price" placeholder="Liquor Price"/>
+          <select ref="status">
+            <option value="available">On The Cellar!</option>
+            <option value="unavailable">Sold Out!</option>
+          </select>
+          <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+          <input type="text" ref="image" placeholder="URL to Image"/>
+          <button type="submit">+ Add Item</button>
+        </form>
+      )
+  }
+});
+
+/*
+  Header
+  <Header/>
+*/
+var Header = React.createClass({
+  render : function() {
+    return (
+      <header className="top">
+        <h1>The Cellar</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3> 
+      </header>
+    )
+  }
+})
+
+/*
+  Barrel
+  <Barrel/>
+*/
+var Barrel = React.createClass({
+  renderBarrel : function(key) {
+    var liquor = this.props.liquors[key];
+    var count = this.props.order[key];
+    var removeButton = <button onClick={this.props.removeFromBarrel.bind(null, key)}>&times;</button>
+    var bottle;
+    if (count > 1)
+        bottle = '-bottles';
+    else
+        bottle = '-bottle';
+    if (!liquor) {
+      return <li key={key}>Liquor no longer available! {removeButton}</li>
+    }
+
+    return (
+      <li key={key}>
+        <span>
+          <CSSTransitionGroup component="span" transitionName="count" transitionLeaveTimeout={250} transitionEnterTimeout={250} className="count">
+            <span key={count}>{count}</span>
+          </CSSTransitionGroup>          
+             
+           {bottle} of {liquor.name} {removeButton}
+
+        </span>
+            <span className="price">{h.formatPrice(count * liquor.price)}</span>                
+      </li>
+      )
+  },
+  render : function() {
+    var orderIds = Object.keys(this.props.order);
+
+    var total = orderIds.reduce((prevTotal, key)=>{
+      var liquor = this.props.liquors[key];
+      var count = this.props.order[key];
+      var isAvailable = liquor && liquor.status === 'available';
+
+      if (liquor && isAvailable) {
+        return prevTotal + (count * parseInt(liquor.price) || 0);
+      }
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+      <h2 className="order-title">Your Barrel</h2>
+
+      <CSSTransitionGroup 
+        className="order" 
+        component="ul" 
+        transitionName="order"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
+
+        {orderIds.map(this.renderBarrel)}
+        <li className='total'>
+          <strong>Total:</strong>
+          {h.formatPrice(total)}
+        </li>    
+      </CSSTransitionGroup>
+      </div>
+    )
+  }
+})
+
+/*
+  Inventory
+  <Inventory/>
+*/
+var Inventory = React.createClass({
+  renderInventory : function(key) {
+    var linkState = this.props.linkState;
+    return (
+      <div className="liquor-edit" key={key}>
+        <input type="text" valueLink={linkState('liquors.' + key +'.name')}/>
+        <input type="text" valueLink={linkState('liquors.' + key +'.price')}/>
+        <select valueLink={linkState('liquors.' + key +'.status')}>
+          <option value="unavailable">Sold Out!</option>
+          <option value="available">On The Cellar!</option>          
+        </select>
+        <textarea valueLink={linkState('liquors.' + key +'.desc')}></textarea>
+        <input type="text" valueLink={linkState('liquors.' + key +'.image')}/>        
+        <button onClick={this.props.removeLiquor.bind(null, key)}>Remove Liquor</button>
+      </div>
+      )
+  },
+  render : function() {
+    return (
+      <div>
+        <h3>Inventory</h3>
+        {Object.keys(this.props.liquors).map(this.renderInventory)}
+        <AddLiquorForm {...this.props} />
+        {/*<AddLiquorForm addLiquor={this.addLiquor} />*/}
+        <button onClick={this.props.loadSamples}>Load Sample Data</button>
+      </div>      
+    )
+  }
+})
+
+
+/* 
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+  mixins : [History],
+  goToStore : function(event) {
+    event.preventDefault();
+    // get data from input
+    var storeId = this.refs.storeId.value;    
+    // transition from <StorePicker/> to <App/>
+    this.history.pushState(null, '/store/' + storeId);
+  },
+  render : function() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+        <input type="Submit" />
+      </form>
+    )
+  }
+
+});
+
+/*
+  Not Found
+*/
+
+var NotFound = React.createClass({
+  render : function() {
+    return <h3><center>Location Not Available!</center></h3>
+  }
+});
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+)
+
+
+ReactDOM.render(routes, document.querySelector('#main'));
