ee6f7c14b96829ac600b770fd3bb3f45b866ad70
Glenn Lizardo <gdlizardo@up.edu.ph>
2017-01-03 18:14:11 +0800

    Separated each component as modules

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var CSSTransitionGroup = require('react-addons-css-transition-group');
 
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
@@ -19,6 +18,18 @@ var base = Rebase.createClass('https://appdevbuddy.firebaseio.com/');
 
 
 var Catalyst = require('react-catalyst');
+
+
+/* Component Imports */
+import NotFound from './components/NotFound';
+import Header from './components/Header';
+import StorePicker from './components/StorePicker';
+import AddLiquorForm from './components/AddLiquorForm';
+import Liquor from './components/Liquor';
+import Barrel from './components/Barrel';
+import Inventory from './components/Inventory';
+
+
 
 /*
   App
@@ -106,238 +117,13 @@ var App = React.createClass({
   }
 });
 
-/*
-  Liquor Component
-*/
-
-var Liquor = React.createClass({
-  onButtonClick : function() {
-    var key = this.props.index;
-    this.props.addToBarrel(this.props.index);
-  },
-  render : function() {
-    var details = this.props.details;
-    var isAvailable = (details.status === 'available' ? true : false);
-    var buttonText = (isAvailable ? 'Add To Barrel' : 'Sold Out');
-    return (
-      <li className="menu-liquor">
-        <image src={details.image} alt={details.name} />
-        <h3 className="liquor-name">
-          {details.name}
-          <span className="price">{h.formatPrice(details.price)}</span>
-        </h3>
-        <p>{details.desc}</p>
-        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
-      </li>
-    )
-  }
-});
-
-/*
-  Add Liquor Form
-*/
-
-var AddLiquorForm = React.createClass({
-  createLiquor : function(event) {
-    // Stop the form from submitting
-    event.preventDefault();
-    // Take the data from the form and create an object
-    var liquor = {
-      name : this.refs.name.value,
-      price : this.refs.price.value,
-      status : this.refs.status.value,
-      desc : this.refs.desc.value,
-      image : this.refs.image.value
-    }
-    // Add the liquor to the App State
-    this.props.addLiquor(liquor);
-    this.refs.liquorForm.reset();
-  },
-  render : function() {
-    return (
-        <form className="liquor-edit" ref="liquorForm" onSubmit={this.createLiquor}>
-          <input type="text" ref="name" placeholder="Liquor Name"/>
-          <input type="text" ref="price" placeholder="Liquor Price"/>
-          <select ref="status">
-            <option value="available">On The Cellar!</option>
-            <option value="unavailable">Sold Out!</option>
-          </select>
-          <textarea type="text" ref="desc" placeholder="Desc"></textarea>
-          <input type="text" ref="image" placeholder="URL to Image"/>
-          <button type="submit">+ Add Item</button>
-        </form>
-      )
-  }
-});
-
-/*
-  Header
-  <Header/>
-*/
-var Header = React.createClass({
-  render : function() {
-    return (
-      <header className="top">
-        <h1>The Cellar</h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3> 
-      </header>
-    )
-  },  
-  propTypes : {
-    tagline : React.PropTypes.string.isRequired
-  }
-})
-
-/*
-  Barrel
-  <Barrel/>
-*/
-var Barrel = React.createClass({
-  renderBarrel : function(key) {
-    var liquor = this.props.liquors[key];
-    var count = this.props.order[key];
-    var removeButton = <button onClick={this.props.removeFromBarrel.bind(null, key)}>&times;</button>
-    var bottle;
-    if (count > 1)
-        bottle = '-bottles';
-    else
-        bottle = '-bottle';
-    if (!liquor) {
-      return <li key={key}>Liquor no longer available! {removeButton}</li>
-    }
-
-    return (
-      <li key={key}>
-        <span>
-          <CSSTransitionGroup component="span" transitionName="count" transitionLeaveTimeout={250} transitionEnterTimeout={250} className="count">
-            <span key={count}>{count}</span>
-          </CSSTransitionGroup>          
-             
-           {bottle} of {liquor.name} {removeButton}
-
-        </span>
-            <span className="price">{h.formatPrice(count * liquor.price)}</span>                
-      </li>
-      )
-  },
-  render : function() {
-    var orderIds = Object.keys(this.props.order);
-
-    var total = orderIds.reduce((prevTotal, key)=>{
-      var liquor = this.props.liquors[key];
-      var count = this.props.order[key];
-      var isAvailable = liquor && liquor.status === 'available';
-
-      if (liquor && isAvailable) {
-        return prevTotal + (count * parseInt(liquor.price) || 0);
-      }
-      return prevTotal;
-    }, 0);
-
-    return (
-      <div className="order-wrap">
-      <h2 className="order-title">Your Barrel</h2>
-
-      <CSSTransitionGroup 
-        className="order" 
-        component="ul" 
-        transitionName="order"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}>
-
-        {orderIds.map(this.renderBarrel)}
-        <li className='total'>
-          <strong>Total:</strong>
-          {h.formatPrice(total)}
-        </li>    
-      </CSSTransitionGroup>
-      </div>
-    )
-  },
-  propTypes : {
-    liquors : React.PropTypes.object.isRequired,
-    order : React.PropTypes.object.isRequired,
-    removeFromBarrel : React.PropTypes.func.isRequired
-  }
-})
-
-/*
-  Inventory
-  <Inventory/>
-*/
-var Inventory = React.createClass({
-  renderInventory : function(key) {
-    var linkState = this.props.linkState;
-    return (
-      <div className="liquor-edit" key={key}>
-        <input type="text" valueLink={linkState('liquors.' + key +'.name')}/>
-        <input type="text" valueLink={linkState('liquors.' + key +'.price')}/>
-        <select valueLink={linkState('liquors.' + key +'.status')}>
-          <option value="unavailable">Sold Out!</option>
-          <option value="available">On The Cellar!</option>          
-        </select>
-        <textarea valueLink={linkState('liquors.' + key +'.desc')}></textarea>
-        <input type="text" valueLink={linkState('liquors.' + key +'.image')}/>        
-        <button onClick={this.props.removeLiquor.bind(null, key)}>Remove Liquor</button>
-      </div>
-      )
-  },
-  render : function() {
-    return (
-      <div className="inventory">
-        <h3>Inventory</h3>
-        {Object.keys(this.props.liquors).map(this.renderInventory)}
-        <AddLiquorForm {...this.props} />
-        {/*<AddLiquorForm addLiquor={this.addLiquor} />*/}
-        <button onClick={this.props.loadSamples}>Load Sample Data</button>
-      </div>      
-    )
-  },
-  propTypes : {
-    addLiquor : React.PropTypes.func.isRequired,
-    loadSamples : React.PropTypes.func.isRequired,
-    liquors : React.PropTypes.object.isRequired,
-    linkState : React.PropTypes.func.isRequired,
-    removeLiquor : React.PropTypes.func.isRequired
-  }
-})
 
 
-/* 
-  StorePicker
-  This will let us make <StorePicker/>
-*/
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event) {
-    event.preventDefault();
-    // get data from input
-    var storeId = this.refs.storeId.value;    
-    // transition from <StorePicker/> to <App/>
-    this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
-    return (
-      <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please Enter A Store</h2>
-        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
-        <input type="Submit" />
-      </form>
-    )
-  }
 
-});
 
-/*
-  Not Found
-*/
 
-var NotFound = React.createClass({
-  render : function() {
-    return <h3><center>Location Not Available!</center></h3>
-  }
-});
+
 
 /*
   Routes
